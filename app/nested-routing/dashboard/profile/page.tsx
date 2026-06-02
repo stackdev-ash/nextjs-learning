1866086945d9e273ea48a9d972977601420d47dc
Ashish Sharma <stackdev.ash@gmail.com>
2026-06-02 18:28:39 +0530

    Learn nested routing in Next.js App Router

---
+++ app/nested-routing/dashboard/profile/page.tsx
@@ -0,0 +1,5 @@
+function Profile() {
+    return <h1> here nesting is happening profile is nested in dashbaord page </h1>;
+}
+
+export default Profile;
---
+++ app/nested-routing/page.tsx
@@ -0,0 +1,5 @@
+function NestedRouting() {
+    return <h1> Nested Routing page </h1>;
+}
+
+export default NestedRouting;
